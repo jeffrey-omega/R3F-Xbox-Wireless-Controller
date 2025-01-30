4ff84596b38fb385956dda936b71af868adaf065
jeffrey-omega <1844253+jeffrey-omega@users.noreply.github.com>
2025-01-30 16:38:54 +0100

    first commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,15 @@
 import { OrbitControls } from "@react-three/drei";
-import { Atom } from "./Atom";
+import { Cameras } from "./Cameras";
+import { SpotLights } from "./SpotLights";
+import { XboxWirelessController } from "./XboxWirelessController";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls enableZoom={true} enablePan={false} />
-      <Atom />
+      <SpotLights />
+      <Cameras />
+      <XboxWirelessController visible={true} />
     </>
   );
 };
